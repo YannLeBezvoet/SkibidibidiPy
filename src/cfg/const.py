9ef249652ef0b193eb359f4bdfa7f093c101e106
YannLeBezvoet <yann.lebezvoet@gmail.com>
2024-04-18 18:53:45 +0200

    feat: new const

--- src/cfg/const.py
+++ src/cfg/const.py
@@ -1,2 +1,6 @@
 class Const:
     outPath = ".out"
+    terminalUsage1 = "SkibidibidiPy by Yann Le Bezvoet - dev 1"
+    terminalUsage2 = "\tUsage : skibidibidipy /path/to/skibidibidiTemplate /path/to/skibidibidiPython/code"
+    terminalOneArg1 = "SkibidibidiPy takes exactly 2 arguments but "
+    terminalOneArg2 = " were given"
--- test/cfg/test_const.py
+++ test/cfg/test_const.py
@@ -1,3 +1,7 @@
 from cfg.const import Const
 def test_varType():
     assert isinstance(Const.outPath, str)
+    assert isinstance(Const.terminalUsage1, str)
+    assert isinstance(Const.terminalUsage2, str)
+    assert isinstance(Const.terminalOneArg1, str)
+    assert isinstance(Const.terminalOneArg2, str)
