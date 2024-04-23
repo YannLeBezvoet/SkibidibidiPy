6467540653f9a550f9936909b3074f3faa0dd422
YannLeBezvoet <yann.lebezvoet@gmail.com>
2024-04-23 17:30:38 +0200

    test: check file when there is a new line

--- test/fileReader/test_loadTemplate.py
+++ test/fileReader/test_loadTemplate.py
@@ -1,7 +1,13 @@
 from fileReader.loadTemplate import loadTemplate
 
-def test_loadTemplate():
-    template = loadTemplate("test/rcs/skibidibidi.template")
+def loadTestTemplate(path):
+    template = loadTemplate(path)
     assert(template.dictonary["🖨️"]=="print")
     assert(template.dictonary["0️⃣"]=="0")
     assert(template.dictonary["🛌"]=="sleep")
+
+def test_loadTemplateNoEndLine():
+    loadTestTemplate("test/rcs/skibidibidi.template")
+
+def test_loadTemplateEndLine():
+    loadTestTemplate("test/rcs/skibidibidiEndLine.template")
